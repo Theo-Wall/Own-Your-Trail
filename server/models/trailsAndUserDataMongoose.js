1d23e3a8ac4aabfbe906b0b8b3d721448c1abbd5
hrycykj <john@quillconsulting.ca>
2021-11-18 15:11:25 -0700

    Mongoose is functional to database

--- server/models/trailsAndUserDataMongoose.js
+++ server/models/trailsAndUserDataMongoose.js
@@ -1,25 +1,25 @@
 const mongoose = require('./mongooseDb')
 
-const Trail = mongoose.model('Trail', {   "userId": Number,
-                                            "trailName": String,
+const Trail = mongoose.model('Trail', {   "userId": Number,                                 // add in required inputs
+                                            "trailName": {type: String, required: true},
                                             "photos": [String],
                                             "trailDescription": String,
                                             "trailMap": String
                                         })
                                         
-const UserData = mongoose.model('UserData', {   "userId": Number,
-                                                "userName": String
+const UserData = mongoose.model('UserData', {   "userId": Number,                               // add in required inputs
+                                                "userName": {type: String, required: true}
                                             })
 
 
-async function createTrail(trailData) {
+async function createTrail(trailData) { //write catch block for errors
     let newTrail = new Trail(trailData)
     console.log (newTrail)
     let createdTrail = await newTrail.save()
     return createdTrail.id
 }
 
-async function createUser(userData) {
+async function createUser(userData) { //write catch block for errors
     let newUser = new UserData(userData)
     console.log (newUser)
     let createdUser = await newUser.save()
@@ -33,16 +33,23 @@ async function createUser(userData) {
 //     return Room.findOne({name: roomName})
 // }
 
-async function listRooms() {
-    return Room.find({})
+async function listTrails() {
+    let returnedData = await Trail.find({})
+    console.log('inside listTrails:',returnedData)
+    return (returnedData)
 }
 
-createUser ({"userId": 1235,
-"userName": 'Dustin and Reza'});
+async function listUsers() {
+    let returnedData = await UserData.find({})
+    console.log('inside listTrails:',returnedData)
+    return (returnedData)
+}
 
-// module.exports = {
-//     createRoom,
+module.exports = {
+    createTrail,
+    createUser,
 //     findRoomById,
 //     findRoomByName,
-//     listRooms
-// }
+    listTrails,
+    listUsers
+}
