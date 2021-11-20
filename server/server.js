c99cd39d76226a4c3d8e69983f5230f5c263e409
theowall <theo.wall11@gmail.com>
2021-11-20 12:30:34 -0700

    changed to port 5001

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const trailAndUserRouter = require('./routes/trailAndUserRoutes')
 
 const app = express()
-const port = 5000
+const port = 5001
 
 app.use('/api',trailAndUserRouter)
 
