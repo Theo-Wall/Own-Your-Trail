8eb68bf10207db1d06705e1eff8db61b0286150e
hrycykj <john@quillconsulting.ca>
2021-11-22 21:18:10 -0700

    added useEffect

--- client/src/components/ListTrails.js
+++ client/src/components/ListTrails.js
@@ -1,7 +1,36 @@
 import './ListTrails.css'
+import { useState, useEffect } from 'react'
+
+// const fetchTrailData = {
+//     userId: 1,
+//     trailName: "the trail1" ,
+//     photos: ["https://www.google.com/maps/d/thumbnail?mid=1f0DqYdEHXIWs25ACkMNiMZ-Wkpw"," https://thePhotoDamIt.com"],
+//     trailDescription: "I walked this trail every day with my grand pappy and so should you.",
+//     trailMap: "turn left at cowboy ave and take a right at lasso st"
+// }
 
 const ListTrails = () => {
+    const [trailData, setTrailData] = useState({})
     
+    useEffect(() => {
+        const fetchTrailData = async () => {
+          let fetchResult = await fetch('/api/listTrails')
+          let fetchedTrails = await fetchResult.json()
+          setTrailData(fetchedTrails[0])  // force trailData to one of the array elements
+        }
+        fetchTrailData()
+      }, [])
+    
+    // async function getTrailsDammit () {
+    //     console.log('ListTrails is working so far.')
+    //     let fetchTrailData = await fetch ('/api/listTrails')
+    //     let fetchedTrailData = await fetchTrailData.json()
+    //     console.log(fetchedTrailData)
+    //     return fetchedTrailData
+    // }
+    // setTrailData (getTrailsDammit ())
+    // // let trailData = data[0]
+    console.log(trailData)
     return (
         <div className='trailsTable'>
             <h3>Trail Title</h3>
@@ -16,10 +45,10 @@ const ListTrails = () => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>trail1</td>
-                        <td>https://www.google.com/maps/d/thumbnail?mid=1f0DqYdEHXIWs25ACkMNiMZ-Wkpw</td>
-                        <td>My dog loves taking dumps by this fir tree.</td>
-                        <td>turn left at cowboy ave and take a right at lasso st</td>
+                        <td>{trailData.trailName}</td>
+                        <td>{trailData.photos}</td>
+                        <td>{trailData.trailDescription}</td>
+                        <td>{trailData.trailMap}</td>
                     </tr>
                 </tbody>
             </table>
