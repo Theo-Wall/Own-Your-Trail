5dd9d9b101e920b82d1cc9fe68a1eca2c60fa0d4
theowall <theo.wall11@gmail.com>
2021-11-27 20:36:09 -0700

    added comment for image display in trailRow

--- client/src/components/TrailRow.js
+++ client/src/components/TrailRow.js
@@ -5,11 +5,20 @@
 //     trailDescription: "I walked this trail every day with my grand pappy and so should you.",
 //     trailMap: "turn left at cowboy ave and take a right at lasso st"
 // }
+
+// import { CloudinaryContext, Image } from 'cloudinary-react'
 const TrailRow = ({ name, photos, description, map }) => {
     return (
         <tr>
             <td>{name}</td>
             <td>{photos}</td>
+            {/* <td>
+                <CloudinaryContext cloudName="ddcynhc98">
+                    <div>
+                        <Image publicId={photos} width="50" />
+                    </div>
+                </CloudinaryContext>
+            </td> */}
             <td>{description}</td>
             <td>{map}</td>
         </tr>
