a7f44df4bdccf6207fc381229351222b2cb69429
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2021-11-29 16:19:45 -0700

    changed navbar style

--- client/src/pages/pageComponents/NavBar.js
+++ client/src/pages/pageComponents/NavBar.js
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom"
 const NavBar = () => {
     return (
         <div className='navigation-bar'>
-            <div >Navigation Bar of Optimum</div>
+            <div className='app-name'><h1>Own Your Trail</h1></div>
             <span>
                 {/* <Link></Link> are used to point to the Routes which point to page components  */}
-            <Link to="/createTrailPage" >Create Trail Page</Link>
+            <Link className='text-link' to="/createTrailPage">Create Trail Page</Link>
             </span>
             <span>
-            <Link to="/">Go Home</Link> 
+            <Link className='text-link' to="/">Go Home</Link> 
             </span>
+            <button>Log In</button>
+            
         </div>
     )
 }
