8d8979efbaee6a029ef093c7b6356c69d6290bb3
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2021-12-01 13:15:41 -0700

    changed font

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <div>
       {/* Header and NavBar should always be outside of the Routes due to the fact that they are always going to be on the page */}
-      <Header />
+  
       <NavBar setTrailId = {setTrailId}/> 
       {/* This is the old way we used to route our pages */}
       {/* {{createTrailState ? <CreateTrailPage /> : <MainPage /> } {/*<MainPage /> : null}*/}
