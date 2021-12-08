45593bfddc2a69899b2e701063c51e6a73dc1a45
theowall <theo.wall11@gmail.com>
2021-12-07 17:56:05 -0700

    added background image

--- client/src/components/ListTrails.js
+++ client/src/components/ListTrails.js
@@ -16,28 +16,28 @@ const ListTrails = () => {
       }, [])
     
     return (
-        
-        <div>
-            <h3 className='trails-table'>Trail Title</h3>              
-                 <div className='scroll-box'>
-                    <div className='card-grid'>
-                        {
-                        trailData.map((trail)=> {
-                            return (
-                                <TrailRow   key={trail._id}
-                                            id={trail._id}
-                                            name={trail.trailName}
-                                            photos={trail.photos}
-                                            primaryPhoto={trail.primaryPhoto}
-                                            description={trail.trailDescription}
-                                            cityQuadrant={trail.cityQuadrant}
-                                /> )
-                            })
-                        }
-                    </div>
-                </div>
+      <div>
+        <div className="trails-table">
+          <div className="scroll-box">
+            <div className="card-grid">
+              {trailData.map((trail) => {
+                return (
+                  <TrailRow
+                    key={trail._id}
+                    id={trail._id}
+                    name={trail.trailName}
+                    photos={trail.photos}
+                    primaryPhoto={trail.primaryPhoto}
+                    description={trail.trailDescription}
+                    cityQuadrant={trail.cityQuadrant}
+                  />
+                );
+              })}
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default ListTrails
