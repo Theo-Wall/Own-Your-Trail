7d50a1cee7524d3816a86cf57a5c538b2a871ea6
theowall <theo.wall11@gmail.com>
2021-12-09 14:07:38 -0700

    messing with styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,25 +5,27 @@ const NavBar = () => {
 
     return (
       <div className="navigation-bar">
-        <div className="app-name">
+        <span className="app-name">
           <h1>Own Your Trail</h1>
-        </div>
-        <span>
+        </span>
+        <span className="links">
+        <span className="nav-link">
           <Link className="text-link" to="/">
             Go Home
           </Link>
         </span>
-        <span>
+        <span className="nav-link">
           {/* <Link></Link> are used to point to the Routes which point to page components  */}
           <Link className="text-link" to="/createTrailPage">
             Create Your Trail
           </Link>
         </span>
-        <span>
+        <span className="nav-link">
           <Link className="text-link" to="">
             Login
           </Link>
         </span>
+      </span>
       </div>
     );
 }
