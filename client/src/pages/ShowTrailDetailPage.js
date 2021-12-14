51b5ffc91940d76588fec0e745f6c3a8193fece1
theowall <theo.wall11@gmail.com>
2021-12-14 16:23:53 -0700

    added star rating to trail detail

--- client/src/pages/ShowTrailDetailPage.js
+++ client/src/pages/ShowTrailDetailPage.js
@@ -3,6 +3,7 @@ import ShowTrailPhoto from "../components/ShowTrailPhoto";
 import LoginScreen from "../components/Login/LoginScreen";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Rating } from "react-simple-star-rating"
 import AuthVerify from '../components/Login/AuthVerify'
 
 let ShowTrailDetailPage = ({ loginScreenState, setLoginScreenState, registrationScreenState, setRegistrationScreenState, token, setToken, isLoggedIn, setIsLoggedIn, userInfo, setUserInfo }) => {
@@ -10,7 +11,7 @@ let ShowTrailDetailPage = ({ loginScreenState, setLoginScreenState, registration
   let trailId = params.id;
 
   const [trailData, setTrailData] = useState([]);
-
+  
   useEffect(() => {
     const fetchTrailData = async () => {
       let fetchResult = await fetch("/api/getTrailInfo/" + trailId);
@@ -20,9 +21,61 @@ let ShowTrailDetailPage = ({ loginScreenState, setLoginScreenState, registration
     fetchTrailData();
   }, [trailId]);
 
+
+  const handleRating = async (rate) => {
+    let newNumberOfRatings = trailData.numberOfTrailRatings + 1
+    let newTrailRating = ((trailData.trailRating * trailData.numberOfTrailRatings + ((rate / 20)-1)) / newNumberOfRatings)
+    console.log(newTrailRating, (rate/20))
+
+      const response = await fetch('/api/updateTrail/' + trailId, {
+        method: "POST",
+        body: JSON.stringify({
+          "trailRating": newTrailRating.toFixed(1),
+          "numberOfTrailRatings": newNumberOfRatings
+        }),
+        headers: {
+          "x-access-token": token,
+          "Content-Type": "application/json",
+        },
+    
+        
+      })
+      let fetchedUpdatedTrail = await response.json()
+      setTrailData(fetchedUpdatedTrail)
+  }
   return (
     <div>
       <h3>{trailData.trailName}</h3>
+      <span>
+        {isLoggedIn ? (
+          <Rating
+            onClick={handleRating}
+            size={20}
+            ratingValue={trailData.trailRating * 20}
+            label
+            transition
+            readonly={false}
+            fillColor="orange"
+            emptyColor="gray"
+            className="foo"
+          />
+        ) : (
+          <Rating
+          
+            size={20}
+            ratingValue={trailData.trailRating * 20}
+            label
+            transition
+            readonly={true}
+            fillColor="orange"
+            emptyColor="gray"
+            className="foo"
+          />
+        )}
+        <span>
+          {trailData.trailRating} ({trailData.numberOfTrailRatings})
+        </span>
+      </span>
       <div>
         {trailData.photos &&
           trailData.photos.map((photo, index) => {
@@ -49,7 +102,7 @@ let ShowTrailDetailPage = ({ loginScreenState, setLoginScreenState, registration
         userInfo={userInfo}
         setUserInfo={setUserInfo}
       />
-      <AuthVerify 
+      <AuthVerify
         token={token}
         setToken={setToken}
         isLoggedIn={isLoggedIn}
