3099e4f93e4df6b0dbc43f92b0325f4898b3fd5c
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2021-12-15 15:21:09 -0700

    worked on show trail page css

--- client/src/components/ShowTrailPhoto.js
+++ client/src/components/ShowTrailPhoto.js
@@ -1,24 +1,26 @@
-import '../pages/ShowTrailDetailPage.css'
+import './ShowTrailPhoto.css'
+
 
 let ShowTrailPhoto = ({photo,photoDescription,onPrimaryPhoto,arrayIndex}) => {
                     return (
-                      <a href={photo} style={{textDecoration: "none"}}>
-                        <span>
-                          <img className='photo-thumbnails' src={photo} alt="trail" />
-                        </span>
-                          {onPrimaryPhoto && 
-                          <div>
-                            <label htmlFor="primePhotoPicker">
-                              <input type="radio" id="primePhotoPicker" name="primaryPhoto" onChange={onPrimaryPhoto} value={arrayIndex} />
-                              Make Display Photo
-                            </label>
-                          </div>
-                          }
-                        <span>
-                          <h5>{photoDescription}</h5>
-                        </span>  
-                        
-                      </a>
+                      <div className='show-trail-photo'>
+                        <a href={photo} style={{textDecoration: "none"}}>
+                          <span className='trail-photo'>
+                            <img className='photo-thumbnails' src={photo} alt="trail" />
+                          </span>
+                            {onPrimaryPhoto && 
+                            <div>
+                              <label htmlFor="primePhotoPicker">
+                                <input type="radio" id="primePhotoPicker" name="primaryPhoto" onChange={onPrimaryPhoto} value={arrayIndex} />
+                                Make Display Photo
+                              </label>
+                            </div>
+                            }
+                          <span className='photo-description'>
+                            <h5>{photoDescription}</h5>
+                          </span>  
+                        </a>
+                      </div>
                     )
                   }
 
--- client/src/pages/ShowTrailDetailPage.js
+++ client/src/pages/ShowTrailDetailPage.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Rating } from "react-simple-star-rating"
 import AuthVerify from '../components/Login/AuthVerify'
+import Card from '../components/ui/Card'
 
 let ShowTrailDetailPage = ({ loginScreenState, setLoginScreenState, registrationScreenState, setRegistrationScreenState, token, setToken, isLoggedIn, setIsLoggedIn, userInfo, setUserInfo }) => {
   let params = useParams();
@@ -69,74 +70,65 @@ let ShowTrailDetailPage = ({ loginScreenState, setLoginScreenState, registration
       setHasNotRated(false)
   }
   return (
-    <div className="centered" >
+    <div className="centered">
+     <div className="site-title">
         <h3>{trailData.trailName}</h3>
-        <div className="photo">
-          {trailData.photos &&
-            trailData.photos.map((photo, index) => {
-              return (
-                <ShowTrailPhoto
+      
+        {isLoggedIn && !isUserTrail && hasNotRated ? (
+          <Rating
+          onClick={handleRating}
+          size={20}
+          ratingValue={trailData.trailRating * 20}
+          label
+          transition
+          readonly={false}
+          fillColor="orange"
+          emptyColor="gray"
+          className="foo"
+          />
+        ) : (
+          <Rating
+          size={20}
+          ratingValue={trailData.trailRating * 20}
+          label
+          transition
+          readonly={true}
+          fillColor="orange"
+          emptyColor="gray"
+          className="foo"
+          />
+        )}
+          <span>
+            {trailData.trailRating} ({trailData.numberOfTrailRatings})
+          </span>
+           {isUserTrail && isLoggedIn ? (
+            <div>This is your trail</div>
+          ) : (
+            <div>This trail was created by: {trailData.userName}</div>
+          )}
+        </div>
+       
+      
+      <div className="photo">
+        {trailData.photos &&
+          trailData.photos.map((photo, index) => {
+            return (
+              <Card>
+                <ShowTrailPhoto 
                   key={photo._id}
                   photo={photo.url}
                   photoDescription={photo.description}
                   arrayIndex={index}
                 />
-              );
-            })}
-        
-    <div className="centered">
-      <h3 className="site-title">{trailData.trailName}</h3>
-      <span>
-        {isLoggedIn && !isUserTrail && hasNotRated ? (
-          <Rating
-            onClick={handleRating}
-            size={20}
-            ratingValue={trailData.trailRating * 20}
-            label
-            transition
-            readonly={false}
-            fillColor="orange"
-            emptyColor="gray"
-            className="foo"
-          />
-        ) : (
-          <Rating
-            size={20}
-            ratingValue={trailData.trailRating * 20}
-            label
-            transition
-            readonly={true}
-            fillColor="orange"
-            emptyColor="gray"
-            className="foo"
-          />
-        )}
-        <span>
-          {trailData.trailRating} ({trailData.numberOfTrailRatings})
-        </span>
-          {isUserTrail && isLoggedIn ? (
-            <div>This is your trail</div>
-          ) : (
-            <div>This trail was created by: {trailData.userName}</div>
-          )}
-      </span>
-      <div className="card">
-        {trailData.photos &&
-          trailData.photos.map((photo, index) => {
-            return (
-              <ShowTrailPhoto
-                key={photo._id}
-                photo={photo.url}
-                photoDescription={photo.description}
-                arrayIndex={index}
-              />
+              </Card>
             );
           })}
-        <div className="description">{trailData.trailDescription}</div>
-        <div className="map">{trailData.trailMap}</div>
       </div>
-          <div>{trailData.trailDescription}</div>
-          <div>{trailData.trailMap}</div>
+          <div className='trail-content'>
+            <div className="description">{trailData.trailDescription}</div>
+            <div className="map">{trailData.trailMap}</div>
+        </div>
+
       <LoginScreen
         loginScreenState={loginScreenState}
         setLoginScreenState={setLoginScreenState}
